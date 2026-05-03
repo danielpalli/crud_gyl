b918e1d0afe19aaa01143e170831f67147bd573a
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2026-05-03 18:12:46 -0300

    Fix/0305/Fix al venta mapper, implementacion venta

--- src/main/java/com/gyl/CrudGyL/mapper/VentaMapper.java
+++ src/main/java/com/gyl/CrudGyL/mapper/VentaMapper.java
@@ -1,15 +1,36 @@
 package com.gyl.CrudGyL.mapper;
 
+import com.gyl.CrudGyL.dto.request.VentaRequestDto;
 import com.gyl.CrudGyL.dto.response.DetalleVentaResponseDto;
 import com.gyl.CrudGyL.dto.response.VentaResponseDto;
 import com.gyl.CrudGyL.entity.DetalleVenta;
 import com.gyl.CrudGyL.entity.Venta;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VentaMapper {
 
     private VentaMapper() {
+    }
+
+    public static Venta toEntity(VentaRequestDto dto) {
+        Venta venta = new Venta();
+        venta.setFechaVenta(LocalDate.now());
+        venta.setTotal(0.0);
+        return venta;
+    }
+
+    public static VentaResponseDto toResponseDto(Venta venta) {
+        return new VentaResponseDto(
+            venta.getIdVenta(),
+            venta.getFechaVenta(),
+            venta.getTotal(),
+            venta.getCliente().getIdCliente(),
+            venta.getCliente().getNombre() + " " + venta.getCliente().getApellido(),
+            new ArrayList<>()
+        );
     }
 
     public static VentaResponseDto toResponseDto(Venta venta, List<DetalleVenta> detalles) {
@@ -25,6 +46,10 @@ public class VentaMapper {
             venta.getCliente().getNombre() + " " + venta.getCliente().getApellido(),
             detallesDto
         );
+    }
+
+    public static void updateEntity(Venta venta, VentaRequestDto dto) {
+        venta.setFechaVenta(LocalDate.now());
     }
 
     public static DetalleVentaResponseDto toDetalleResponseDto(DetalleVenta detalle) {
--- src/main/java/com/gyl/CrudGyL/service/impl/VentaServiceImpl.java
+++ src/main/java/com/gyl/CrudGyL/service/impl/VentaServiceImpl.java
@@ -2,6 +2,12 @@ package com.gyl.CrudGyL.service.impl;
 
 import com.gyl.CrudGyL.dto.request.VentaRequestDto;
 import com.gyl.CrudGyL.dto.response.VentaResponseDto;
+import com.gyl.CrudGyL.entity.Cliente;
+import com.gyl.CrudGyL.entity.Venta;
+import com.gyl.CrudGyL.exception.RecursoNoEncontradoException;
+import com.gyl.CrudGyL.mapper.VentaMapper;
+import com.gyl.CrudGyL.repository.ClienteRepository;
+import com.gyl.CrudGyL.repository.VentaRepository;
 import com.gyl.CrudGyL.service.VentaService;
 import org.springframework.stereotype.Service;
 
@@ -9,30 +15,63 @@ import java.util.List;
 
 @Service
 public class VentaServiceImpl implements VentaService {
+    private VentaRepository repository;
+    private ClienteRepository clienteRepository;
 
+    public VentaServiceImpl(VentaRepository repository, ClienteRepository clienteRepository) {
+        this.repository = repository;
+        this.clienteRepository = clienteRepository;
+    }
 
     @Override
     public VentaResponseDto crear(VentaRequestDto dto) {
-        return null;
+        Venta venta = VentaMapper.toEntity(dto);
+        Cliente cliente = clienteRepository.findById(dto.idCliente()).orElse(null);
+        venta.setCliente(cliente);
+
+        Venta guardado = repository.save(venta);
+        return VentaMapper.toResponseDto(guardado);
     }
 
     @Override
     public List<VentaResponseDto> listar() {
-        return List.of();
+        return repository.findAll()
+            .stream()
+            .map(VentaMapper::toResponseDto)
+            .toList();
     }
 
     @Override
     public VentaResponseDto buscarPorId(Long id) {
-        return null;
+        return repository.findById(id)
+            .map(VentaMapper::toResponseDto)
+            .orElseThrow(() -> new RecursoNoEncontradoException(
+                "No se encontro el id: " + id
+            ));
     }
 
     @Override
     public VentaResponseDto actualizar(Long id, VentaRequestDto dto) {
-        return null;
+        Venta venta = repository.findById(id)
+            .orElseThrow(() -> new RecursoNoEncontradoException(
+                "No se encontro el id" + id
+            ));
+
+        VentaMapper.updateEntity(venta, dto);
+        Cliente cliente = clienteRepository.findById(dto.idCliente()).orElse(null);
+        venta.setCliente(cliente);
+
+        Venta guardado = repository.save(venta);
+        return VentaMapper.toResponseDto(guardado);
     }
 
     @Override
     public void eliminar(Long id) {
+        Venta venta = repository.findById(id)
+            .orElseThrow(() -> new RecursoNoEncontradoException(
+                "No se encontro el id" + id
+            ));
 
+        repository.delete(venta);
     }
 }
