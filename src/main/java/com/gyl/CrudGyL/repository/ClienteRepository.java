053d38df62c158fb16611ad36ff8f29fe6a32c4b
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2026-05-04 01:27:51 -0300

    Fix/0405/No permitir crear clientes con el mismo correo

--- src/main/java/com/gyl/CrudGyL/repository/ClienteRepository.java
+++ src/main/java/com/gyl/CrudGyL/repository/ClienteRepository.java
@@ -4,6 +4,10 @@ import com.gyl.CrudGyL.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    boolean existsByCorreo(String correo);
+    boolean existsByCorreoAndIdClienteNot(String correo, Long id);
 }
