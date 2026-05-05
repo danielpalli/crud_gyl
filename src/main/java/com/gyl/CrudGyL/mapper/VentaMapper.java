f610be0dee42404574638bd92184c94049a99c44
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2026-05-05 00:40:00 -0300

    Fix/0505/No usaba el update de mapper Venta

--- src/main/java/com/gyl/CrudGyL/mapper/VentaMapper.java
+++ src/main/java/com/gyl/CrudGyL/mapper/VentaMapper.java
@@ -1,6 +1,5 @@
 package com.gyl.CrudGyL.mapper;
 
-import com.gyl.CrudGyL.dto.request.VentaRequestDto;
 import com.gyl.CrudGyL.dto.response.DetalleVentaResponseDto;
 import com.gyl.CrudGyL.dto.response.VentaResponseDto;
 import com.gyl.CrudGyL.entity.Cliente;
@@ -9,7 +8,6 @@ import com.gyl.CrudGyL.entity.Venta;
 import com.gyl.CrudGyL.mapper.config.GlobalMapperConfig;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
 import java.util.List;
@@ -27,13 +25,6 @@ public interface VentaMapper {
     DetalleVentaResponseDto toDetalleDto(DetalleVenta detalleEntity);
 
     List<DetalleVentaResponseDto> toDetalleDtoList(List<DetalleVenta> listEntity);
-
-    @Mapping(target = "idVenta", ignore = true)
-    @Mapping(target = "fechaVenta", ignore = true)
-    @Mapping(target = "total", ignore = true)
-    @Mapping(target = "cliente", ignore = true)
-    @Mapping(target = "detalles", ignore = true)
-    void updateEntity(@MappingTarget Venta venta, VentaRequestDto dto);
 
     @Named("combinarNombres")
     default String combinar(Cliente cliente) {
