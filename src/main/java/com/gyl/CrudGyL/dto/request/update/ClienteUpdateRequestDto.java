532b44829c942a4c9b4acc268bea23e63546a4f3
Daniel Palli <feerhh91@gmail.com>
2026-05-06 15:12:43 -0300

    Feature/0605/Agregado update dto

---
+++ src/main/java/com/gyl/CrudGyL/dto/request/update/ClienteUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.gyl.CrudGyL.dto.request.update;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ClienteUpdateRequestDto(
+    @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
+    String nombre,
+
+    @Size(max = 50, message = "El apellido no puede tener más de 50 caracteres")
+    String apellido,
+
+    @Email(message = "Debe ser un correo electrónico válido")
+    @Size(max = 150, message = "El correo electrónico no puede tener más de 150 caracteres")
+    String correo,
+
+    @Pattern(regexp = "^(\\d{8}\\d{10})$", message = "El teléfono solo deber tener 8 o 10 dígitos numéricos")
+    String telefono,
+
+    @Size(max = 150, message = "La dirección no puede tener más de 150 caracteres")
+    String direccion
+){}
--- src/main/java/com/gyl/CrudGyL/dto/request/update/TipoProductoUpdateRequestDto.java
+++ src/main/java/com/gyl/CrudGyL/dto/request/update/TipoProductoUpdateRequestDto.java
@@ -1,14 +1,12 @@
-package com.gyl.CrudGyL.dto.request;
+package com.gyl.CrudGyL.dto.request.update;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record TipoProductoRequestDto(
-    @NotBlank(message = "El nombre no puede estar vacío")
-    @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
-    String nombre,
+public record TipoProductoUpdateRequestDto(
+    @Size(max = 50, message = "El nombre del tipo de producto no puede tener más de 50 caracteres")
+    String nombreTipoProducto,
 
-    @NotBlank(message = "La descripción no puede estar vacía")
     @Size(max = 100, message = "La descripción no puede tener más de 100 caracteres")
     String descripcion
 ) {
