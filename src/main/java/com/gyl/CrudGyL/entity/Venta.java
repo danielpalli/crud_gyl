91786511daa6fc4eeb2e5a9f17c41d398b473b88
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2026-05-08 02:36:37 -0300

    Feature/0805/Agregado builder a las entidades

--- src/main/java/com/gyl/CrudGyL/entity/Venta.java
+++ src/main/java/com/gyl/CrudGyL/entity/Venta.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
