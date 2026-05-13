91524223ee0df01c51d090ca3c813c9a7acc6663
Daniel Palli <feerhh91@gmail.com>
2026-05-13 11:27:13 -0300

    Fix/1305/Fix error de mapeo de password

--- src/main/java/com/gyl/CrudGyL/mapper/UsuarioMapper.java
+++ src/main/java/com/gyl/CrudGyL/mapper/UsuarioMapper.java
@@ -3,13 +3,12 @@ package com.gyl.CrudGyL.mapper;
 import com.gyl.CrudGyL.dto.request.LoginRequestDto;
 import com.gyl.CrudGyL.dto.request.RegistroRequestDto;
 import com.gyl.CrudGyL.entity.Usuario;
-import com.gyl.CrudGyL.enums.Role;
 import com.gyl.CrudGyL.mapper.config.GlobalMapperConfig;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(config = GlobalMapperConfig.class, imports = Role.class)
+@Mapper(config = GlobalMapperConfig.class)
 public interface UsuarioMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(source = "password", target = "password")
